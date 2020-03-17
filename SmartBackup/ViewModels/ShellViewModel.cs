5557583c1f677c2c76ecb7fa93a734ff33c41e8a
Ken Li <abc00gh@hotmail.com>
2020-03-16 23:04:52 -0600

    Init Project

---
+++ SmartBackup/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SmartBackup.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<DemoViewModel>(), new CancellationToken());
+        }
+    }
+}
